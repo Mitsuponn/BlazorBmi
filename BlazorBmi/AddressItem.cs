71264942cc8be09bbd3df912284db9eb49830741
imamura <mi.imamura.mitsuharu@gmail.com>
2021-01-14 21:24:29 +0900

    [Add&Change] AdressComp+ MenuLink

---
+++ BlazorBmi/AddressItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBmi
+{
+    public class AddressItem
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public string zipcode { get; set; }
+        public string telephone { get; set; }
+        public int prefectureId { get; set; } = 13;
+    }
+}
